dafd7d154290634ecb16bc10154f1a03317f626c
kienpros213 <kienpros213@gmail.com>
2023-10-08 17:13:16 +0700

    added room feature

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -17,44 +17,70 @@ export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnec
     console.log('A user disconnect');
   }
 
-  @SubscribeMessage('draw')
-  handleClientEvent(client: Socket, payload: any): void {
-    console.log(payload);
-    this.drawState.push(payload.point)
-    this.server.to(payload.roomName).emit('draw', payload.point);
-  }
 
   @SubscribeMessage('joinRequest')
   handleJoinRequest(client: Socket, roomName: any): void {
     client.join(roomName)
-    console.log(roomName)
   }
 
   @SubscribeMessage('clientBrushDraw')
   handleClientBrushDraw(client: Socket, payload: any): void {
-    console.log(payload);
-    this.drawState.push(payload);
-    client.broadcast.emit("serverBrushDraw", payload);
-  }
+    console.log("brush:", payload)
+    // client.broadcast.emit("serverBrushDraw", payload);
+    client.to(payload.room).emit("serverBrushDraw", payload)
+  } 
 
   @SubscribeMessage('clientCircleDraw')
   handleClientCircleDraw(client: Socket, payload: any): void {
-    console.log(payload);
-    client.broadcast.emit("serverCircleDraw", payload);
+    // client.broadcast.emit("serverCircleDraw", payload);
+    client.to(payload.room).emit("serverCircleDraw", payload)
+  }
+
+  @SubscribeMessage('clientPushCircle')
+  handleClientPushCircle(client: Socket, payload: any): void {
+    // client.broadcast.emit("serverPushCircle", payload);
+    client.to(payload.room).emit("serverPushCircle", payload)
   }
 
   @SubscribeMessage('clientRectDraw')
   handleClientRectDraw(client: Socket, payload: any): void {
-    console.log(payload);
-    client.broadcast.emit("serverRectDraw", payload);
+    // client.broadcast.emit("serverRectDraw", payload);
+    client.to(payload.room).emit("serverRectDraw", payload)
+  }
+
+  @SubscribeMessage('clientPushRect')
+  handleClientPushRect(client: Socket, payload: any): void {
+    // client.broadcast.emit("serverPushRect", payload);
+    client.to(payload.room).emit("serverPushRect", payload)
   }
 
   @SubscribeMessage('clientEraserDraw')
   handleClientEraserDraw(client: Socket, payload: any): void {
-    console.log(payload);
-    client.broadcast.emit("serverEraserDraw", payload);
+    // client.broadcast.emit("serverEraserDraw", payload);
+    client.to(payload.room).emit("serverEraserDraw", payload)
   }
 
+  @SubscribeMessage('clientFreeShapeDraw')
+  handleClientFreeShapeDraw(client: Socket, payload: any): void {
+    console.log(payload)
+    // client.broadcast.emit("serverFreeShapeDraw", payload);
+    client.to(payload.room).emit("serverFreeShapeDraw", payload)
+  }
+
+  @SubscribeMessage('clientStopFreeShape')
+  handleClientStopFreeShape(client: Socket, payload: any): void {
+    // client.broadcast.emit("serverStopFreeShape",);
+    console.log("stop")
+    client.to(payload.room).emit("serverStopFreeShape", payload);
+  }
+
+  
+  @SubscribeMessage('sendSnapshot')
+  handleSendSnapshot(client: Socket, payload: any): void {
+    console.log(payload);
+    // client.broadcast.emit("serverSendSnapshot", payload);
+    client.to(payload.room).emit("serverSendSnapshot", payload);
+  }
   
 
 }
