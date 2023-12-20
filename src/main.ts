ae9efdde1c8bc6479de213dc17e70c5f926819ff
kienpros213 <kienpros213@gmail.com>
2023-12-20 23:03:30 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter());
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // Replace with the actual origin of your frontend
+    origin: 'https://p5-websocket.vercel.app/', // Replace with the actual origin of your frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true // Set to true if you need to allow cookies with CORS
   });
